138a04588fc954ad6b981e04886e9eb0cbbb6f7e
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-13 13:50:30 +0000

    replaced Flask with Hug

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask, send_from_directory, render_template, request
+import hug
 
-app = Flask(__name__, static_folder='static')
-
-@app.route('/', methods=['GET', 'POST'])
-def base():
-    return render_template('cubis_home.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+@hug.get('/')
+def root():
+    return 'Hi'
