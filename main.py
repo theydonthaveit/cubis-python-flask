60c0eeecd30123666370422373998e4ef683ff58
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-14 08:32:16 +0000

    test send data for hug api

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import hug
 @hug.get('/')
 def root():
     return 'Hi'
+
+@hug.get('/restaurants/')
+def list_restaurants(output=hug.output_format.json):
+    restaurants = [{
+        'name': 'Goat House',
+        'seating': 50,
+        'location': 'SE1 6DE',
+        'cusine': 'British Goats Only'
+    },{
+        'name': 'Jungle Boot',
+        'seating': 25,
+        'location': 'SE7 6FF',
+        'cusine': 'British Jungle Boots Only'
+    }]
+    return restaurants
