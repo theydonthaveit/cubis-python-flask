7c74154ede7d7b8d4063040b957a5da92af2698b
theydonthaveit <theydonthaveit@gmail.com>
2018-02-18 19:46:02 +0000

    hug and flask api versions

--- main_hug.py
+++ main_hug.py
@@ -1,11 +1,9 @@
-from flask import Flask, send_from_directory, render_template, request, redirect, url_for, flash, jsonify
+import hug
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Restaurant, Base
-
-app = Flask(__name__, static_folder='static')
-app.secret_key = 'super secret key'
 
 Engine = create_engine('postgres://orajzfxrmovnsa:5b924f7d92c7faa14a0eafc35d9b05af4f62981f66583757a53513162f8a9081@ec2-54-247-101-191.eu-west-1.compute.amazonaws.com:5432/dsnmc7ts3e869')
 Base.metadata.create_all(Engine)
@@ -13,14 +11,12 @@ Base.metadata.create_all(Engine)
 DBsession = sessionmaker(bind=Engine)
 session = DBsession()
 
-@app.route('/')
+@hug.get('/')
 def root():
     return 'Hi'
 
-@app.route('/restaurants/', methods=['GET'])
-def list_restaurants():
+@hug.get('/restaurants/')
+def list_restaurants(output=hug.output_format.json):
     restaurants = session.query(Restaurant).all()
-    return jsonify(Restaurants = [r.serialize for r in restaurants])
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    Restaurants = [r.serialize for r in restaurants]
+    return Restaurants
