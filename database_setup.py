da5f91bad7aeef9571427516589511a65bc8b607
Alan Williams <theydonthaveit@gmail.com>
2018-03-23 12:19:10 +0000

    thigns have happened

--- database_setup.py
+++ database_setup.py
@@ -1,55 +1,75 @@
 import sys
+import datetime
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from flask_login import LoginManager, UserMixin
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.hash import pbkdf2_sha512
 
 Base = declarative_base()
 
 
-class Restaurant(Base):
-	__tablename__ = 'restaurant'
+class BaseMixin(object):
 
-	id = Column(Integer, primary_key=True)
+    @declared_attr
+    def id(self):
+        return Column(Integer, primary_key=True, unique=True)
+
+    @declared_attr
+    def created_at(self):
+        return Column(DateTime, default=datetime.datetime.utcnow)
+
+    @declared_attr
+    def updated_at(self):
+        return Column(DateTime, default=datetime.datetime.utcnow)
+
+
+
+class UserAccount(Base, BaseMixin, UserMixin):
+    __tablename__ = 'user_account'
+
+    def __init__(self, username, passcode, mobile, email, ip_address):
+        self.username = username
+        self.password = pbkdf2_sha512.hash(passcode)
+        self.mobile = mobile
+        self.email = email
+        self.ip_address = ip_address
+
+    username = Column(String(80), nullable=False)
+    password = Column(String(1000), nullable=False)
+    mobile = Column(String(15), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=True)
+    ip_address = Column(String(15), nullable=False)
+    is_verified = Column(Boolean, nullable=False, default=False)
+
+    def is_verified_check(self):
+        return self.is_verified
+
+    def decode_password(self, password):
+        return pbkdf2_sha512.verify(password, self.password)
+
+
+class Restaurant(Base, BaseMixin):
+    	__tablename__ = 'restaurant'
+
 	name = Column(String(120), nullable=False)
+	user = relationship(UserAccount, back_populates="user_account")
+	user_id = Column(Integer, ForeignKey('user_account.id'))
 
 
-	@property
-	def serialize(self):
-		return {
-			'id': self.id,
-			'name': self.name,
-			'_links': {
-				'self': {
-					'rel': 'Display Restaurant name and corresponding content',
-					'href': '/restaurant/{id}/'
-				}
-			}
-		}
-
-
-class SubRestaurant(Base):
+class SubRestaurant(Base, BaseMixin):
     __tablename__ = 'subsidiary_restaurant'
 
-    id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
     restaurant = relationship(Restaurant, back_populates="subsidiary_restaurant")
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 
-    @property
-    def serialize(self):
-	    return {
-			'id': self.id,
-			'name': self.name,
-			'parent_restaurant': self.restaurant,
-		}
 
-
-class Address(Base):
+class Address(Base, BaseMixin):
     __tablename__ = 'addresses'
 
-    id = Column(Integer, primary_key=True)
     subsidiary_restaurant = relationship(SubRestaurant, back_populates="addresses")
     subsidiary_restaurant_id = Column(Integer, ForeignKey('subsidiary_restaurant.id'))
     address_line_one = Column(String(80), nullable=False)
@@ -60,25 +80,10 @@ class Address(Base):
     long = Column(String(80), nullable=False)
     lat = Column(String(80), nullable=False)
 
-    @property
-    def serialize(self):
-	    return {
-			'id': self.id,
-			'subsidiary_restaurant': self.subsidiary_restaurant,
-            'address_line_one': self.address_line_one,
-		    'address_line_two': self.address_line_two,
-		    'city': self.city,
-		    'country': self.country,
-            'postcode': self.postcode,
-			'long': self.long,
-			'lat': self.lat
-		}
 
-
-class RestaurantInfo(Base):
+class RestaurantInfo(Base, BaseMixin):
     __tablename__ = 'restaurant_info'
 
-    id = Column(Integer, primary_key=True)
     subsidiary_restaurant = relationship(SubRestaurant, back_populates="restaurant_info")
     subsidiary_restaurant_id = Column(Integer, ForeignKey('subsidiary_restaurant.id'))
     addresses = relationship(Address, back_populates="restaurant_info")
@@ -86,36 +91,10 @@ class RestaurantInfo(Base):
     seating = Column(Integer, nullable=False)
     cuisine = Column(String(100), nullable=False)
 
-    @property
-    def serialize(self):
-	    return {
-			'id': self.id,
-			'subsidiary_restaurant': self.subsidiary_restaurant,
-			'addresses': self.addresses,
-			'seating': self.seating,
-			'cuisine': self.cuisine,
-		}
-
-
-# # class RestaurantQueue(Base):
-
-
-# # class RestaurantTransactions(Base):
-
-
-# # class RestaurantReviews(Base):
-
-
-# # class RestaurantDisputes(Base):
-
-
-# # class Menu(Base):
-
 
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
+    	__tablename__ = 'menu_item'
 
-	id = Column(Integer, primary_key=True)
 	course = Column(String(250))
 	description = Column(String(500))
 	price = Column(String(8))
@@ -123,15 +102,7 @@ class MenuItem(Base):
 	subsidiary_restaurant = relationship(SubRestaurant, back_populates="menu_item")
 	subsidiary_restaurant_id = Column(Integer, ForeignKey('subsidiary_restaurant.id'))
 
-	@property
-	def serialize(self):
-		return {
-			'id': self.id,
-			'course': self.course,
-			'description': self.description,
-			'price': self.price,
-			'restaurant': self.subsidiary_restaurant
-		}
 
-engine = create_engine('postgres://orajzfxrmovnsa:5b924f7d92c7faa14a0eafc35d9b05af4f62981f66583757a53513162f8a9081@ec2-54-247-101-191.eu-west-1.compute.amazonaws.com:5432/dsnmc7ts3e869')
-Base.metadata.create_all(engine)
+
+Engine = create_engine('postgres://uzmbizckonpkrz:f98d7f3d375ce1cbdde41b455baa3586054bcf82caa6facb5d82dfacf9f3050a@ec2-79-125-12-27.eu-west-1.compute.amazonaws.com:5432/d833fj11bma730')
+Base.metadata.create_all(Engine)
--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-from flask import Flask, send_from_directory, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant
+from database_setup import Base, Engine, Restaurant, UserAccount
 # SubRestaurant, Address, RestaurantInfo, MenuItem
 
-Engine = create_engine('postgres://orajzfxrmovnsa:5b924f7d92c7faa14a0eafc35d9b05af4f62981f66583757a53513162f8a9081@ec2-54-247-101-191.eu-west-1.compute.amazonaws.com:5432/dsnmc7ts3e869')
 Base.metadata.create_all(Engine)
 
 DBsession = sessionmaker(bind=Engine)
 session = DBsession()
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 @app.route('/')
 def root():
-    return 'Hi'
+    return 'HOME'
 
-@app.route('/restaurants/', methods=['GET'])
+@app.route('/restaurants/list', methods=['GET'])
 def list_restaurants():
-    restaurants = session.query(Restaurant).all()
-    return jsonify(Restaurants = [r.serialize for r in restaurants])
+    return render_template('restaurants.html')
 
 
-@app.route('/restaurants/JSON')
-def list_restaurants():
-    restaurant = session.query(Restaurant).all()
-    return jsonify(Restaurant=[i.serialize for i in restaurant])
+@app.route('/restaurant_dashboard/<int: restaurant_id>')
+def restaurant_dashboard(restaurant_id):
+    
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
